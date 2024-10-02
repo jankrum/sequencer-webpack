83d9dab2a58c2331718638d500f90f2217753c35
jankrum <professionalninety@gmail.com>
2024-10-02 09:42:25 -0700

    Made transporter work

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-// main.js
-import { pipe, take, map, forEach } from 'callbag-basics'
-import { workerSource } from 'callbag-worker'
+// // main.js
+// import { pipe, take, map, forEach } from 'callbag-basics'
+// import { workerSource } from 'callbag-worker'
+
+// pipe(
+//     workerSource(new Worker(new URL('./worker.js', import.meta.url))), // or workerSource('worker.js')
+//     take(5),
+//     map(x => x / 2),
+//     forEach(x => console.log(x))
+// )
+// // 0.5
+// // 1.5
+// // 2.5
+// // 3.5
+// // 4.5
+
+import { pipe, map, forEach } from 'callbag-basics'
+import fromEvent from 'callbag-from-event'
+import merge from 'callbag-merge'
+
+const transporter$ = (() => {
+    return merge(
+        ...[
+            'play',
+            'pause',
+            'stop',
+        ].map((name) => pipe(
+            fromEvent(document.querySelector(`#${name}`), 'click'),
+            map(() => name)
+        ))
+    )
+})()
 
 pipe(
-    workerSource(new Worker(new URL('./worker.js', import.meta.url))), // or workerSource('worker.js')
-    take(5),
-    map(x => x / 2),
+    transporter$,
     forEach(x => console.log(x))
 )
-// 0.5
-// 1.5
-// 2.5
-// 3.5
-// 4.5
--- src/worker.js
+++ src/worker.js
@@ -1,10 +1,10 @@
-// worker.js
-import { pipe, filter } from 'callbag-basics'
-import interval from 'callbag-interval'
-import { workerSink } from 'callbag-worker'
+// // worker.js
+// import { pipe, filter } from 'callbag-basics'
+// import interval from 'callbag-interval'
+// import { workerSink } from 'callbag-worker'
 
-pipe(
-    interval(1000),
-    filter(x => x % 2),
-    workerSink(self)
-)
+// pipe(
+//     interval(1000),
+//     filter(x => x % 2),
+//     workerSink(self)
+// )
